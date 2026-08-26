ee9b5821b0fcdcbaecc15e64d494f51b7f71c23d
Nadira Harakow <nadirah@metropolia.fi>
2026-08-26 11:34:18 +0300

    typo fix

--- Harjoittelu/hello.py
+++ Harjoittelu/hello.py
@@ -7,3 +7,5 @@ print("Hauska tavata, " + käyttäjä + "!")
 
 # Perustyypit: merkkijono (string), luku (number), joka voi olla kokonaisluku (int), liukuluku (float) tai kopleksiluku, totuusarvo (boolean, joka voi olla true tai false)
 # Muut pythonin tietorakenteet: lista (list), monikko (tuple) ja sanakirja (dictionary).
+
+
---
+++ mod04/01kuha.py
@@ -0,0 +1,7 @@
+pituus = float(input("Anna kuhan pituus senttimetreinä: "))
+
+if pituus < 37:
+    puuttuu = 37 - pituus
+    print("Laske kuha takaisin järveen!")
+    print(f"Se on {puuttuu} senttiä alamittainen.")
+    
