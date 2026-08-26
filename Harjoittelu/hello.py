6da9fc291e62f98d76c2d6d67b2e15576056f39a
Nadira Harakow <nadirah@metropolia.fi>
2026-08-26 10:18:13 +0300

    Typo fix

---
+++ Harjoittelu/hello.py
@@ -0,0 +1,9 @@
+käyttäjä = input('Anna nimesi: ')
+print("Hauska tavata, "+ käyttäjä + "!")
+
+Väri = "Vihreä"
+
+print("Hauska tavata, " + käyttäjä + "!")
+
+# Perustyypit: merkkijono (string), luku (number), joka voi olla kokonaisluku (int), liukuluku (float) tai kopleksiluku, totuusarvo (boolean, joka voi olla true tai false)
+# Muut pythonin tietorakenteet: lista (list), monikko (tuple) ja sanakirja (dictionary).
